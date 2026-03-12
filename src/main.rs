4c4cc3b32ab6c2385d80cc5e813e1d856dd95156
Valentin Weber <valentin@wv2.ch>
2026-03-12 17:21:24 +0100

    fix: wring description of git dimensions

--- src/main.rs
+++ src/main.rs
@@ -268,7 +268,7 @@ fn generate_payload(args: &mut Args) -> Result<PayloadBuffer> {
                 let frame_count = frames.len();
                 let (width, height) = frames.first().unwrap().buffer().dimensions();
                 if height != DISPLAY_HEIGHT || width != DISPLAY_WIDTH {
-                    anyhow::bail!("Expected {DISPLAY_WIDTH}x{DISPLAY_WIDTH} pixel gif file");
+                    anyhow::bail!("Expected {DISPLAY_WIDTH}x{DISPLAY_HEIGHT} pixel gif file");
                 }
 
                 let mut buffer = payload.add_message(
